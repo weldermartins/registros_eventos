f725fc48e491d45bb8cbf305fccf083005ff96c9
weldermartins <weldermartins@gmail.com>
2022-06-02 13:31:16 -0300

    gitignore

--- registro_aprendizado/settings.py
+++ registro_aprendizado/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Minha aplicações
+    # Minha aplicações 2
     'registros_aprendizados'
 ]
 
